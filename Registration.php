4cb258175c69263809f4e9d09eca715c01ca5b25
arbaz-kh <47318938+arbaz-kh@users.noreply.github.com>
2019-03-08 10:57:55 +0500

    Cookies and Session Added

--- Registration.php
+++ Registration.php
@@ -6,7 +6,22 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
+<?php
 
+session_start();
+
+if(isset($_POST['btnSubmit']))
+{
+	$_SESSION['uname'] = $_POST['txtUname'];
+	setcookie("email",$_POST['txtEmail']);
+	setcookie("password",$_POST['txtPsw']);
+	setcookie("contact",$_POST['txtCnt']);
+	header("Location: Output.php");
+		
+}
+
+
+?>
 <body>
 
 
@@ -25,7 +40,7 @@
     
     <div class="row">
     	<div class="col-md-4">
-        	<form method="post" action="Output.php">
+        	<form method="post">
               <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
                 <input type="email" class="form-control" name="txtEmail" aria-describedby="emailHelp" placeholder="Enter email">               
